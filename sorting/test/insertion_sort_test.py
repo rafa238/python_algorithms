7a358e72e2e5df7e066d6d33b179f122592b54fe
rafa238 <rafalaureano246@gmail.com>
2023-01-27 12:20:52 -0600

    Custom messages for test

--- sorting/test/insertion_sort_test.py
+++ sorting/test/insertion_sort_test.py
@@ -1,9 +1,10 @@
 from load_module import *
 from sorting.insertion_sort import finsertion_sort
+from custom_messages import show_success_message, show_failed_message
 
 if __name__ == "__main__":
     A = [5,2,4,6,1,3]
     sorted_A = finsertion_sort(A)
     
-    assert sorted_A == [1,2,3,4,5,6], "Error: Insertion Sort Test Failed"
-    print("✓ Insertion Sort algorithm test success")
+    assert sorted_A == [1,2,3,4,5,6], show_failed_message("Insertion Sort Test Failed")
+    show_success_message("Insertion Sort algorithm test success")
